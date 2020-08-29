7e9836652d5c759c987b4398e4f9dcede8252070
urguru <39453452+urguru@users.noreply.github.com>
2020-08-29 23:58:19 +0530

    Added functions

---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    apiKey: "AIzaSyBOrX3N_iISNdszTmpS2ZvtQVtLMLIUrJM",
+    authDomain: "socialape-4e778.firebaseapp.com",
+    databaseURL: "https://socialape-4e778.firebaseio.com",
+    projectId: "socialape-4e778",
+    storageBucket: "socialape-4e778.appspot.com",
+    messagingSenderId: "949488609182",
+    appId: "1:949488609182:web:0a0288c0ca065af4a8588f",
+    measurementId: "G-03T67E0KY9"
+};
